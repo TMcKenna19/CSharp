bd7641a71d01529fe2db97e94c736ca05551c6b5
tmckenna19 <tdlmckenna@gmail.com>
2021-11-29 22:06:04 -0600

    CSharp

---
+++ fundamentals/basic13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace basic13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PrintNumbers();
+        }
+        //print all of the intergers from 1 to 255
+        public static void PrintNumbers(){
+            for(int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    
+}
---
+++ fundamentals/basic13/basicinJS.js
@@ -0,0 +1,4 @@
+//print all of the intergers from 1 to 255
+// function PrintNumbers(){
+//     for
+// }
